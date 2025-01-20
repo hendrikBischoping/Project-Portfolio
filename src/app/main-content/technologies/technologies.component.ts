52f1cfdd6557d3758d9f1331b49668502a7801f5
hendrikBischoping <h.bischoping@gmx.de>
2025-01-21 00:21:34 +0100

    styled 'about me'-section plus added @for-loop to render 'technologies'

--- src/app/main-content/technologies/technologies.component.ts
+++ src/app/main-content/technologies/technologies.component.ts
@@ -5,8 +5,44 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './technologies.component.html',
-  styleUrl: './technologies.component.scss'
+  styleUrl: './technologies.component.scss',
 })
 export class TechnologiesComponent {
-
+  technologies: {
+    name: string; // needs to fit the exact name of image (png)!
+  }[] = [
+    {
+      name: 'HTML',
+    },
+    {
+      name: 'CSS',
+    },
+    {
+      name: 'JavaScript',
+    },
+    {
+      name: 'Material Design',
+    },
+    {
+      name: 'TypeScript',
+    },
+    {
+      name: 'Angular',
+    },
+    {
+      name: 'Firebase',
+    },
+    {
+      name: 'Git',
+    },
+    {
+      name: 'Rest-Api',
+    },
+    {
+      name: 'Scrum',
+    },
+    {
+      name: 'Growth Mindset',
+    },
+  ]
 }
