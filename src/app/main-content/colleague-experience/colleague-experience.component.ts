48a0eda85476e380f35bbcbf0bd4e5a9f34cfec6
hendrikBischoping <h.bischoping@gmx.de>
2025-01-23 01:14:26 +0100

    added navigation-arrows to 'colleague-experience'-section incl. mouse-enter/leave-function

--- src/app/main-content/colleague-experience/colleague-experience.component.ts
+++ src/app/main-content/colleague-experience/colleague-experience.component.ts
@@ -9,25 +9,58 @@ import { Commentaries } from '../../interfaces/commentaries.interface';
   styleUrl: './colleague-experience.component.scss'
 })
 export class ColleagueExperienceComponent {
+  arrowHover = false;
+  rightHover = false;
+
+  arrows: {
+    neutral: string;
+    hovered: string;
+    isHovered: boolean;
+  }[] = [
+    {
+      neutral: 'left',
+      hovered: 'left-hover',
+      isHovered: false,
+    },
+    {
+      neutral: 'right',
+      hovered: 'right-hover',
+      isHovered: false,
+    },
+  ]
 
   commentaries: Commentaries[] = [
     {
-      commentary: "Imagine this would be a very laudatory comment",
+      commentary: "Imagine this would be a very laudatory comment + Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quia maiores temporibus consectetur alias sed accusamus corrupti ullam, odit tempora.",
       name: "Mr. X",
       title: "friendly dude",
       isShown: false,
     },
     {
-      commentary: "This could be another very laudatory comment",
+      commentary: "This could be another very laudatory comment + Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quia maiores temporibus consectetur alias sed accusamus corrupti ullam, odit tempora.",
       name: "You know who",
       title: "professional human",
       isShown: true,
     },
     {
-      commentary: "This is where your very laudatory comment about me could be",
+      commentary: "This is where your very laudatory comment about me could be + Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quia maiores temporibus consectetur alias sed accusamus corrupti ullam, odit tempora.",
       name: "Hendrik",
       title: "portfolio owner",
       isShown: false,
     },
   ]
+
+  addHover(index: number){
+    if (!this.arrows[index].isHovered) {
+    this.arrows[index].isHovered = true;
+    console.log( this.arrows[index].isHovered);
+  }
+}
+
+removeHover(index: number){
+    if (this.arrows[index].isHovered) {
+      this.arrows[index].isHovered = false;
+      console.log( this.arrows[index].isHovered);
+    }
+  }
 }
