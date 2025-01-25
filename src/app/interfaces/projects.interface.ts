8e3bc750ebb581fa971f46fa743065a5984cd300
hendrikBischoping <h.bischoping@gmx.de>
2025-01-25 22:19:49 +0100

    replaces hard coded project-table-rows by @for-loop plus changed array 'projectImgs' to 'projects' incl interface

---
+++ src/app/interfaces/projects.interface.ts
@@ -0,0 +1,10 @@
+export interface Projects {
+    name: string;
+    class: string;      //also for img-path
+    isHovered: boolean;
+    isOpened: boolean;
+    github: string;
+    lifetest: string;
+    description: string;
+    technologies: string[];
+}
--- src/app/main-content/portfolio-projects/portfolio-projects.component.ts
+++ src/app/main-content/portfolio-projects/portfolio-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Projects } from './../../interfaces/projects.interface';
 
 @Component({
   selector: 'app-portfolio-projects',
@@ -26,11 +27,53 @@ export class PortfolioProjectsComponent {
     },
   ]
 
+  projects: Projects[] = [
+    {
+      name: 'Pokédex',
+      class: 'pokédex',
+      isHovered: false,
+      isOpened: false,
+      github: 'https://github.com/hendrikBischoping/pok-dex',
+      lifetest: 'https://hendrik-bischoping.developerakademie.net/project-pok%C3%A9dex/index.html',
+      description: 'This is a Pokédex. The project fetches data from an API to render it into the code',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+
+    },
+    {
+      name: 'Sharkie',
+      class: 'sharkie',
+      isHovered: false,
+      isOpened: false,
+      github: 'https://github.com/hendrikBischoping/sharky-game',
+      lifetest: 'https://hendrik-bischoping.developerakademie.net/project%20sharky-game/index.html',
+      description: 'This is a "swim-and-shoot"-game based on object-orinted coding.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+
+    },
+    {
+      name: 'Next Project',
+      class: 'hourglass',
+      isHovered: false,
+      isOpened: false,
+      github: '',
+      lifetest: '',
+      description: 'My next project will apear soon!',
+      technologies: ['not finished jet'],
+
+    },
+  ]
+
   showImage(index: number){
-    if (this.projectImgs[index].visible) {
-      this.projectImgs[index].visible = false;
+    if (this.projects[index].isHovered) {
+      this.projects[index].isHovered = false;
     } else {
-      this.projectImgs[index].visible = true;
+      this.projects[index].isHovered = true;
+      
     }
+  }
+  
+  openProjektDetails(index: number){
+    console.log('Bild ',index);
+    
   }
 }
