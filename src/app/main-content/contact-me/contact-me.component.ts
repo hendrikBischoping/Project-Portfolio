7ece7de66e044ecd7c1650cc740e51aeba375f0d
hendrikBischoping <h.bischoping@gmx.de>
2025-01-28 21:53:36 +0100

    added form-validation to contact-section

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    checkbox: false,
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+    
+  }
 }
